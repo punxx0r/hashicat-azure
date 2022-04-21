c9a2750530b6fc5ba55094091fed92f31940d4eb
Jacob <punxx0r@gmail.com>
2022-04-21 19:44:35 +0000

    Readded Department tag to azure vm

--- main.tf
+++ main.tf
@@ -104,7 +104,6 @@ resource "azurerm_public_ip" "catapp-pip" {
 
 resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
-  Department          = "devops"
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
@@ -136,7 +135,9 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    Department = "devops"
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
